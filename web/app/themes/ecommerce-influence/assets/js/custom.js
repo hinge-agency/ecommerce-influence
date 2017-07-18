dae1cae569ed045c47ebf8915870ac687226a0d8
Martin Kiss <martin@wearehinge.com>
2017-07-18 17:46:20 +0100

    Responsive Navigation

--- web/app/themes/ecommerce-influence/assets/js/custom.js
+++ web/app/themes/ecommerce-influence/assets/js/custom.js
@@ -3,7 +3,16 @@ $(function(){
 	$('input, textarea').placeholder();
 
 	$('.topDripForm__closeButton').on('click', function(e){
+		e.preventDefault();
 		$('.topDripForm__inner').addClass('topDripForm__inner--close');
+	});
+
+	// Responsive navigation menu toggle
+	$('.masthead__right-responsive-link').on('click', function(e){
+		e.preventDefault();
+
+		$('.main').toggleClass('nav-active');
+		$(this).toggleClass('active');
 	});
 });
 /* HTML5 Placeholder jQuery Plugin - v2.3.1
--- web/app/themes/ecommerce-influence/src/js/custom/app.js
+++ web/app/themes/ecommerce-influence/src/js/custom/app.js
@@ -3,6 +3,15 @@ $(function(){
 	$('input, textarea').placeholder();
 
 	$('.topDripForm__closeButton').on('click', function(e){
+		e.preventDefault();
 		$('.topDripForm__inner').addClass('topDripForm__inner--close');
+	});
+
+	// Responsive navigation menu toggle
+	$('.masthead__right-responsive-link').on('click', function(e){
+		e.preventDefault();
+
+		$('.main').toggleClass('nav-active');
+		$(this).toggleClass('active');
 	});
 });
