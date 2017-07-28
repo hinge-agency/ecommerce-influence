be2e52e546a614ebafee31ca2092213c10a9206e
Martin Kiss <martin@wearehinge.com>
2017-07-28 16:00:22 +0100

    Load 3 latest posts into home page

--- web/app/themes/ecommerce-influence/page--home.php
+++ web/app/themes/ecommerce-influence/page--home.php
@@ -5,12 +5,28 @@ Template Name: Home Page Template
 
 use Timber\Timber;
 use Lumberjack\PostTypes\Post;
+use Lumberjack\Functions\Blocks;
+
 
 $context = Timber::get_context();
+
+/* PAGE */
+
 $post = new Post();
+$blocks = new Blocks();
 
 $context['post'] = $post;
 $context['title'] = $post->title;
 $context['content'] = $post->content;
+
+/* EPISODES */
+
+$episodes = Timber::get_posts(array(
+    'post_type' => 'episode',
+    'posts_per_page' => 3,
+    'orderby' => 'post__in'
+));
+
+$context['episodes'] = $episodes;
 
 Timber::render('home.twig', $context);
