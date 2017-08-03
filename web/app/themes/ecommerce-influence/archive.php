4929c0cc047b77938bd3264705716ee0c5200ddf
Martin Kiss <martin@wearehinge.com>
2017-08-03 18:21:55 +0100

    Archive Template for Filter and Pagination

--- web/app/themes/ecommerce-influence/archive.php
+++ web/app/themes/ecommerce-influence/archive.php
@@ -42,5 +42,60 @@ if (is_day()) {
 // TODO: Currently only works for posts, fix for custom post types
 $data['posts'] = Post::query();
 
+//BASE VARIABLES
+$url_base = '';
+$posts_per_page = 9;
+$data['allPosts'] = false;
+
+//ALL POSTS 
+$count_posts = wp_count_posts()->publish;
+
+//POSTS IN CATEGORY
+$cat = get_queried_object();
+$count_cat = $cat->count;
+
+//CHECK IF SEARCH FOR ALL OR CATEGORY
+if ($count_cat){
+	$count_pages = ceil($count_cat / $posts_per_page);
+	$url_base = $url_base . '/' . get_option('category_base');
+}else{
+	$count_pages = ceil($count_posts / $posts_per_page);
+	$url_base = $url_base . '/posts/';
+
+	$data['allPosts'] = true;
+}
+
+//URL CONSTRACT FOR PAGINATION
+$data['next_page'] = '#';
+$data['prev_page'] = '#';
+
+$cat_name = $cat->slug;
+$data['current_page'] = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+if ($data['current_page'] == 1){
+
+	if ($count_pages > 1){
+		$data['next_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] + 1);
+	}
+
+}elseif ($data['current_page'] < $count_pages){
+
+	$data['next_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] + 1);
+	$data['prev_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] - 1);
+
+}elseif ($data['current_page'] == $count_pages){
+
+	$data['prev_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] - 1);
+
+}
+
+$archive_posts = Timber::get_posts(array(
+    'posts_per_page' => $posts_per_page,
+    'orderby' => 'post__in',
+    'category_name' => $cat_name,
+    'paged' => $data['current_page']
+));
+
+$data['archive_posts'] = $archive_posts;
 
 Timber::render($templates, $data);
