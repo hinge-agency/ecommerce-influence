091d1e02c7871dae0b99b5445a636738a8cd05df
Martin Kiss <martin@wearehinge.com>
2017-08-04 14:18:45 +0100

    Add Search option + integrate with filters and pagination

--- web/app/themes/ecommerce-influence/archive.php
+++ web/app/themes/ecommerce-influence/archive.php
@@ -39,62 +39,90 @@ if (is_day()) {
     array_unshift($templates, 'archive-'.get_post_type().'.twig');
 }
 
+$searchQuery = get_search_query();
+$count_found = 0;
+$data['search'] = '';
+
 // TODO: Currently only works for posts, fix for custom post types
 $data['posts'] = Post::query();
 
 //BASE VARIABLES
 $url_base = '';
 $posts_per_page = 9;
+$count_pages = 0;
 $data['allPosts'] = false;
 
 //ALL POSTS 
-$count_posts = wp_count_posts()->publish;
+$getPosts = wp_count_posts();
+$count_posts = ($getPosts ? $getPosts->publish : 0);
 
 //POSTS IN CATEGORY
 $cat = get_queried_object();
-$count_cat = $cat->count;
+$count_cat = ($cat ? $cat->count : '');
 
 //CHECK IF SEARCH FOR ALL OR CATEGORY
-if ($count_cat){
-	$count_pages = ceil($count_cat / $posts_per_page);
-	$url_base = $url_base . '/' . get_option('category_base');
-}else{
-	$count_pages = ceil($count_posts / $posts_per_page);
-	$url_base = $url_base . '/posts/';
+if($searchQuery){
 
-	$data['allPosts'] = true;
+    $query_searched_posts = [
+        'posts_per_page' => -1,
+        's' => $searchQuery
+    ];
+
+    $data['search'] = '?s=' . $searchQuery;
+
+    $found = Timber::get_posts($query_searched_posts); 
+    $count_found = count($found);
+
+    if ($count_found){
+        $count_pages = ceil($count_found / $posts_per_page);
+    }
+
+}elseif ($count_cat){
+    $count_pages = ceil($count_cat / $posts_per_page);
+    $url_base = $url_base . '/' . get_option('category_base');
+}else{
+    $count_pages = ceil($count_posts / $posts_per_page);
+    $url_base = $url_base . '/posts/';
+
+    $data['allPosts'] = true;
 }
 
 //URL CONSTRACT FOR PAGINATION
 $data['next_page'] = '#';
 $data['prev_page'] = '#';
 
-$cat_name = $cat->slug;
+$cat_name = ($cat ? $cat->slug : '');
 $data['current_page'] = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
 if ($data['current_page'] == 1){
 
-	if ($count_pages > 1){
-		$data['next_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] + 1);
-	}
+    if ($count_pages > 1){
+        $data['next_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] + 1);
+    }
 
 }elseif ($data['current_page'] < $count_pages){
 
-	$data['next_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] + 1);
-	$data['prev_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] - 1);
+    $data['next_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] + 1);
+    $data['prev_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] - 1);
 
 }elseif ($data['current_page'] == $count_pages){
 
-	$data['prev_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] - 1);
+    $data['prev_page'] = $url_base . $cat_name . '/page/' . ($data['current_page'] - 1);
 
 }
 
-$archive_posts = Timber::get_posts(array(
+$query_args = [
     'posts_per_page' => $posts_per_page,
     'orderby' => 'post__in',
     'category_name' => $cat_name,
     'paged' => $data['current_page']
-));
+];
+
+if ($searchQuery){
+    $query_args['s'] = $searchQuery;
+}
+
+$archive_posts = Post::query($query_args);
 
 $data['archive_posts'] = $archive_posts;
 
